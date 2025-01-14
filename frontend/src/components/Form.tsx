4b99d4e4de9a517b8c8795e71f186caac3dbd96b
jorditer <jordi.terradas6@gmail.com>
2025-01-14 22:00:28 +0100

    done

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -2,23 +2,32 @@
 // import "react-datetime/css/react-datetime.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
-import Datepicker from "react-datepicker"
+import Datepicker from "react-datepicker";
 
 const Form = () => {
-	// const initDate = new Date().toISOString().split('T')[0] + 'T17:00'; //for datetime-local
-	const initDate = new Date();
-	const [selectedDate, setSelectedDate] = useState<Date | null>(initDate)
+  const initDate = new Date().toISOString().split("T")[0] + "T17:00"; //for datetime-local
+  // const initDate = new Date();
+  const [selectedDate, setSelectedDate] = useState(initDate);
   return (
     <div className="text-base -my-1 flex justify-end flex-col">
-      {/* <label className="">Date</label> */}
-			<Datepicker showTimeSelect dateFormat="MMMM d, yyyy h:mm aa" selected={selectedDate} onChange={(date) => setSelectedDate(date)} />
-			{/* <input className="w-[85%] mx-auto" type="datetime-local" value={selectedDate} onChange={e => setSelectedDate(e.target.value)} name="" id="" /> */}
-      <label className="">Title</label>
-      <input type="text" name="" id="" />
-      <label className="">Place</label>
-      <input type="text" name="" id="" />
-      <label>Description</label>
-      <textarea name="" id=""></textarea>
+      <form action="">
+        <label className="">Date</label>
+        {/* <Datepicker showTimeSelect dateFormat="MMMM d, yyyy h:mm aa" selected={selectedDate} onChange={(date) => setSelectedDate(date)} /> */}
+        <input
+          className=""
+          type="datetime-local"
+          value={selectedDate}
+          onChange={(e) => setSelectedDate(e.target.value)}
+          name=""
+          id=""
+        />
+        <label className="">Title</label>
+        <input type="text" name="" id="" />
+        <label className="">Place</label>
+        <input type="text" name="" id="" />
+        <label>Description</label>
+        <textarea name="" placeholder="What is the plan?" id=""></textarea>
+      </form>
     </div>
   );
 };
