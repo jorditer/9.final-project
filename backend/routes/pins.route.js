508a145257c873815138a8c38571d0699622ce12
jorditer <jordi.terradas6@gmail.com>
2025-01-20 12:03:44 +0100

    stable

--- backend/routes/pins.route.js
+++ backend/routes/pins.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 // import { getPin, postPin, updatePin, deletePin } from "../controllers/pin.controller.js";
-import { postPin, getPin } from "../controllers/pin.controller.js"
+import { deletePin, postPin, getPin } from "../controllers/pin.controller.js"
 
 const router = express.Router();
 
 router.get('/', getPin);
 router.post('/', postPin);
 // router.put('/:id', updatePin);
-// router.delete('/:id', deletePin);
+router.delete('/:id', deletePin);
 
 export default router;
